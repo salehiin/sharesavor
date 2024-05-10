74b85add7e8c5cfd87af7834f2d70032638f25bd
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-05-10 16:08:36 +0600

    data fetch to client side done

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,7 +3,7 @@
 const FoodCard = () => {
     return (
         <div>
-{/*             
+            
                 <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800 mx-auto">
                     <div className="flex justify-center -mt-16 md:justify-end">
                         <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"></img>
@@ -16,7 +16,7 @@ const FoodCard = () => {
                     <div className="flex justify-end mt-4">
                         <a href="#" className="text-lg font-medium text-blue-600 dark:text-blue-300" tabIndex="0" role="link">John Doe</a>
                     </div>
-                </div> */}
+                </div>
                 
             
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+
 import Carousel from '../components/Carousel';
 import Featured from '../components/Featured';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+
+    const foods = useLoaderData()
+    console.log(foods);
+
     return (
         <div>
             <Carousel></Carousel>
