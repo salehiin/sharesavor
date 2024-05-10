d5c3ecfd03ac1ab1e3fd62756db545189e0731db
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-05-10 14:49:20 +0600

    featured section structured

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,7 +4,7 @@ import FoodCard from "./FoodCard";
 const Featured = () => {
     return (
         <div className='flex p-6'>
-            <div className='w-1/2 bg-black text-white p-4'>
+            {/* <div className='w-1/2 bg-black text-white p-4'>
                 <h1 className='text-6xl font bold mb-4'>Featured Foods</h1>
                 <p className='text-lg'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam possimus dolores modi libero! Nobis accusantium recusandae omnis impedit temporibus soluta consectetur repellendus officia adipisci? Vero recusandae, voluptates ut soluta dolorem exercitationem quos? Officiis nemo dolorum voluptatibus temporibus illo reiciendis atque odio repudiandae fuga eius neque blanditiis, voluptate saepe repellat iure cumque consequuntur omnis sapiente quidem cupiditate quis? Ullam, ea quis beatae dignissimos possimus enim. Facilis, magni in sunt ad debitis quasi ab rem ipsa accusamus unde veritatis earum amet provident cumque, itaque quis quidem aliquid odit quas error tempore tenetur ratione vero mollitia! Optio velit hic sequi distinctio eligendi reprehenderit assumenda, sed a qui temporibus, magni error enim mollitia cumque, impedit quis? Eius perferendis nihil ea ipsum veniam totam libero animi deserunt cumque cum est nobis, assumenda sapiente recusandae minus atque illo, esse odit. Sunt, quidem cum dolore dicta alias non delectus accusamus perferendis excepturi qui, itaque veritatis odit id culpa, possimus nisi voluptas expedita ratione commodi saepe sapiente debitis maxime ducimus soluta? Beatae aliquam architecto amet modi. Voluptatum animi magnam, eveniet, nihil assumenda consequatur quo, architecto velit quidem deleniti explicabo id dolor voluptates exercitationem sequi sapiente quae optio ab? Est deleniti eveniet provident fugiat dolor ab maiores quia fuga.
@@ -16,7 +16,7 @@ const Featured = () => {
             </div>
             <div className='w-1/2 bg-green mx-auto'>
                 <FoodCard></FoodCard>
-            </div>
+            </div> */}
         </div>
     );
 };
--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,7 +3,7 @@
 const FoodCard = () => {
     return (
         <div>
-            
+{/*             
                 <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800 mx-auto">
                     <div className="flex justify-center -mt-16 md:justify-end">
                         <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"></img>
@@ -14,48 +14,10 @@ const FoodCard = () => {
                     <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
 
                     <div className="flex justify-end mt-4">
-                        <a href="#" class="text-lg font-medium text-blue-600 dark:text-blue-300" tabindex="0" role="link">John Doe</a>
+                        <a href="#" className="text-lg font-medium text-blue-600 dark:text-blue-300" tabIndex="0" role="link">John Doe</a>
                     </div>
-                </div>
-                <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800 mx-auto">
-                    <div className="flex justify-center -mt-16 md:justify-end">
-                        <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"></img>
-                    </div>
-
-                    <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">Design Tools</h2>
-
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-
-                    <div className="flex justify-end mt-4">
-                        <a href="#" class="text-lg font-medium text-blue-600 dark:text-blue-300" tabindex="0" role="link">John Doe</a>
-                    </div>
-                </div>
-                <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800 mx-auto">
-                    <div className="flex justify-center -mt-16 md:justify-end">
-                        <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"></img>
-                    </div>
-
-                    <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">Design Tools</h2>
-
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-
-                    <div className="flex justify-end mt-4">
-                        <a href="#" class="text-lg font-medium text-blue-600 dark:text-blue-300" tabindex="0" role="link">John Doe</a>
-                    </div>
-                </div>
-                <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800 mx-auto">
-                    <div className="flex justify-center -mt-16 md:justify-end">
-                        <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"></img>
-                    </div>
-
-                    <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">Design Tools</h2>
-
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-
-                    <div className="flex justify-end mt-4">
-                        <a href="#" class="text-lg font-medium text-blue-600 dark:text-blue-300" tabindex="0" role="link">John Doe</a>
-                    </div>
-                </div>
+                </div> */}
+                
             
         </div>
     );
